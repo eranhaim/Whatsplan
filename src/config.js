0361e8b408536eb253c80cc68b7fc8e2ddeb3457
Eran Haim <eranhaim321@gmail.com>
2025-05-19 00:38:43 +0300

    updated backend URL port

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const config = {
-    API_BASE_URL:
-        process.env.REACT_APP_API_URL ||
-        "https://dashing-highly-panda.ngrok-free.app",
+    API_BASE_URL: process.env.REACT_APP_API_URL || "https://104.248.16.192:443",
 };
 
 export default config;
