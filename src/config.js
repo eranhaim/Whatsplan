bd91540d3f4919ebb46c794a280dedf114ac005a
Eran Haim <eranhaim321@gmail.com>
2025-05-19 22:00:54 +0300

    back-end got a new domain

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: process.env.REACT_APP_API_URL || "https://104.248.16.192:443",
+    API_BASE_URL: process.env.REACT_APP_API_URL || "https://what2plan.com",
 };
 
 export default config;
