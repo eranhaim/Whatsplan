63af8d0a842b91f65e4a65e6317ca01dd3d878a0
Eran Haim <eranhaim321@gmail.com>
2025-05-29 11:41:44 +0300

    updated back-end url to be https://what2plan.com instead of localhost:100

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: process.env.REACT_APP_API_URL || "http://localhost:100",
+    API_BASE_URL: process.env.REACT_APP_API_URL || "https://what2plan.com",
 };
 
 export default config;
